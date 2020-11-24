d5af398804cc1b0ad219e167ec97ad92852e0124
KirstLMCG <kirsty.mcglynn@essentialinsurance.co.uk>
2020-11-24 18:06:12 +0000

    Configure back-end in React and rendered Blog to display data

--- src/api.jsx
+++ src/api.jsx
@@ -3,6 +3,6 @@
 import axios from 'axios'
 
 export const getBlogs = () => {
-    return axios.get('https://f2f1c0aaf7de41d4bc57354de1d10938.vfs.cloud9.eu-west-1.amazonaws.com/blog')
+    return axios.get('https://localhost:8081/blog')
 }
 
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,38 +1,40 @@
 import React, { Component } from 'react'
-import { getBlogs } from '../api.jsx'
+// import { getBlogs } from '../api.jsx'
+import axios from 'axios'
 
-class Blog extends Component {
-    state = { blogs: [], err: null}
-    
-  componentDidMount() {  
-      getBlogs()
-        .then(response => {
-            this.setState({blogs: response.data.blog})
-        }).catch(({response}) => {
-            this.setState({
-                err: {
-                    status: response.status, msg: response.data.msg
-                }
-            })
-        })
+export default class Blog extends React.Component {
+  state = {
+    blogs: []
   }
-  
+
+  componentDidMount() {
+    axios.get(`https://f2f1c0aaf7de41d4bc57354de1d10938.vfs.cloud9.eu-west-1.amazonaws.com/blog`)
+      .then(res => {
+          console.log(res.data)
+          this.setState({blogs: res.data})
+      })
+      .catch(err => {
+          console.log(err)
+      })
+  }
+
   render() {
-      const { blogs } = this.state
-      return (
-          <div>
-            <ul>
-              {blogs.map((singleBlog) =>
-                  <li>
-                  {singleBlog.blog}
-                  </li>
-              )}
-            </ul>
-          
-          </div>
-          )
+      const {blogs} = this.state;
+        return (
+      <div>
+      <p className="blog-header">Blogs</p>
+      <p className="blog-description">Hi</p>
+        <ul className="blog-ul">
+        {blogs.map((blog) => (
+              <li key={blog.id}>
+                <div className="blog-title">{blog.title}</div>
+                <div className="blog-description">{blog.description}</div>
+              </li>
+            ))}
+        </ul> 
+      </div>
+    )
   }
 }
 
 
-export default Blog;
