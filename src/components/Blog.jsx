24b77af16967825b6fda41d253f60aaa6502bcf9
KirstLMCG <kirsty.mcglynn@essentialinsurance.co.uk>
2020-11-25 10:40:22 +0000

    Pull data and display for /foodtracking ans basic styling

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -10,7 +10,7 @@ export default class Blog extends Component {
   componentDidMount() {
     axios.get(`https://f2f1c0aaf7de41d4bc57354de1d10938.vfs.cloud9.eu-west-1.amazonaws.com/blog`)
       .then(res => {
-          console.log(res.data)
+          // console.log(res.data)
           this.setState({blogs: res.data})
       })
       .catch(err => {
@@ -21,15 +21,15 @@ export default class Blog extends Component {
   render() {
       const {blogs} = this.state;
         return (
-      <div>
+      <div className="blog-container">
       <p className="blog-header">Blogs</p>
       <p className="blog-description">Hi</p>
         <ul className="blog-ul">
-        {blogs.map((blog) => (
-              <li key={blog.id}>
-                <div className="blog-title">{blog.title}</div>
-                <div className="blog-description">{blog.description}</div>
-              </li>
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <div className="blog-title">{blog.title}</div>
+              <div className="blog-description">{blog.description}</div>
+            </li>
             ))}
         </ul> 
       </div>
--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,10 +1,45 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Food extends Component {
-    render(){
+    state = {
+        foods: []
+    }
+    
+    componentDidMount() {
+        axios.get(`https://f2f1c0aaf7de41d4bc57354de1d10938.vfs.cloud9.eu-west-1.amazonaws.com/foodtracking`)
+        .then(res => {
+         // console.log(res.data)
+          this.setState({foods: res.data})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    
+    render() {
+      const { foods } = this.state;
+      
         return (
-            <p>Test!</p>
+          <div>
+            <ul className="foods-ul">
+              {foods.map((food) => (
+                <div>
+                <li key={food.id}>
+                  <div className="breakfast-title">Breakfast:</div>
+                  <div className="breakfast-item">{food.breakfast}</div>
+                  <div className="lunch-title">Lunch:</div>
+                  <div className="lunch-item">{food.lunch}</div>
+                  <div className="dinner-title">Dinner: </div>
+                  <div className="dinner-item">{food.dinner}</div>
+                  <div className="snacks-title">Snacks:</div>
+                  <div className="snacks-item">{food.snacks}</div> 
+                </li>
+                </div>
+              ))}
+            </ul>
+          </div>
+            
             )
     }
 }
