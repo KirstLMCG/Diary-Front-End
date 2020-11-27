b2852cdf534effd910f51c6eb90ccbddb972ecf6
KirstLMCG <kirsty.mcglynn@essentialinsurance.co.uk>
2020-11-27 09:46:01 +0000

    Add back button and basic styling

--- src/components/SingleBlog.jsx
+++ src/components/SingleBlog.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { getBlog } from '../Api.jsx'
+import { Link } from '@reach/router'
 
 export default class SingleBlog extends Component {
   state = {
@@ -22,10 +23,13 @@ export default class SingleBlog extends Component {
 
       return (
         <main>
-          <div className="single-blog-container">
-          <div className="single-blog-title">
+          <Link to={`/`}>
+            <button className="single-blog-back-btn"> Back </button>
+          </Link>
+            <div className="single-blog-container">
+            <div className="single-blog-title">
              {new Date(blog.title).toLocaleDateString()}
-          </div>
+            </div>
             <div className="single-blog-description"> 
              {blog.description}
             </div> 
